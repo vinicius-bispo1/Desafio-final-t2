d4077c67461eb8be8fc377be56677ac0ac82c9b8
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-11-29 15:39:40 -0300

    adicionei o quero doar e livros doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,10 +1,37 @@
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livro} alt="" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={livro} alt="" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article> 
+                <article>
+                    <img src={livro} alt="" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={livro} alt="" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>   
+            </section>
         </section>
     )
 }
